5a1b0ac48c58454fc5acd1252410ffa1a01c4e9a
sladememarian <apmemarian@gmail.com>
2026-06-29 00:09:34 +0330

    fix: use Azure Cosmos DB connection string env var

--- helpers/database.js
+++ helpers/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
+  const uri = process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGO_DB_URL;
   mongoose
-    .connect(process.env.MONGO_DB_URL)
+    .connect(uri)
     .then(() => console.log("Database Connected"))
     .catch((err) => {
       console.log(err);
